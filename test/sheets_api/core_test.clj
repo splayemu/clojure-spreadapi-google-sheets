8ac74eb0084589ceb610fce8e76554ec3a3092f3
Spencer Apple <splayemu@gmail.com>
2025-02-21 13:12:15 -0800

    updates

--- test/sheets_api/core_test.clj
+++
@@ -1,5 +0,0 @@
-(ns sheets-api.core-test
-  (:require [clojure.test :refer :all]))
-
-(deftest placeholder-test
-  (testing "Placeholder test, replace with actual tests"))
---
+++ test/sheets_api/spreadapi_test.clj
@@ -0,0 +1,6 @@
+(ns sheets-api.spreadapi-test
+  (:require [clojure.test :refer :all]
+            [sheets-api.spreadapi :as spreadapi]))
+
+(deftest placeholder-test
+  (testing "Placeholder test, replace with actual tests"))
